7bf54b440d5a5f5377b6260c2560bdebfc5e33d6
poewmuh <grandow78@gmail.com>
2025-09-14 14:56:13 +0900

    initial commit

---
+++ Assets/Game/Gameplay/Scripts/Helpers/ColorsHelper.cs
@@ -0,0 +1,12 @@
+
+
+using UnityEngine;
+
+namespace Game.Gameplay.Helpers
+{
+    public static class ColorsHelper
+    {
+        public static Color DEFAULT_TILE_COLOR = new Color(1, 1, 1, .12f);
+        public static Color HIGHLIGHT_TILE_COLOR = new Color(1, 1, 1, .25f);
+    }
+}
